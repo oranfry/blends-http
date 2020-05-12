1e64d52f232d4dc2281aaae0bda63c90f70a65e4
Oran Fry <oran@emerg.co>
2020-05-13 11:49:59 +1200

    Stop loading parents this way

--- src/php/controller/line/info.php
+++ src/php/controller/line/info.php
@@ -1,14 +1,4 @@
 <?php
-$linetype = Linetype::load(LINETYPE_NAME);
-$parents = Linetype::find_parent_linetypes(LINETYPE_NAME);
-$parenttypes = [];
-
-foreach ($parents as $parent) {
-    $parenttypes[] = preg_replace('/.*\\\\/', '', get_class($parent));
-}
-
-$linetype->parenttypes = $parenttypes;
-
 return [
-    'data' => $linetype,
+    'data' => Linetype::load(LINETYPE_NAME),
 ];
