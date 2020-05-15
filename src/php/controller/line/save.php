2ef543585fe00949e8bdefa3e2a2beef41df3985
Oran Fry <oran@emerg.co>
2020-05-16 09:52:47 +1200

    Add support for keepfiledata flag

--- src/php/controller/line/save.php
+++ src/php/controller/line/save.php
@@ -1,7 +1,8 @@
 <?php
 $linetype = Linetype::load(LINETYPE_NAME);
 $lines = json_decode(file_get_contents('php://input'));
+$keep_filedata = @$_GET['keepfiledata'] === '1';
 
 return [
-    'data' => $linetype->save($lines),
+    'data' => $linetype->save($lines, 0, null, $keep_filedata),
 ];
