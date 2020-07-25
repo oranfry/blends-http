f56dbadb69b81247d4ab669c3812061395227c65
Oran Fry <oran@emerg.co>
2020-07-25 12:58:24 +1200

    Alphanumeric IDs
    Change routes to support A/N IDs.

--- src/php/class/blendshttp/Router.php
+++ src/php/class/blendshttp/Router.php
@@ -6,9 +6,9 @@ class Router extends \Router
     protected static $routes = [
         'DELETE /([a-z]+)' => ['LINETYPE_NAME', 'PAGE' => 'line/delete'],
         'DELETE /blend/([a-z]+)/delete' => ['BLEND_NAME', 'PAGE' => 'blend/delete'],
-        'GET /([a-z]+)/([0-9]+)' => ['LINETYPE_NAME', 'LINE_ID', 'PAGE' => 'line/index'],
-        'GET /([a-z]+)/([0-9]+)/html' => ['LINETYPE_NAME', 'LINE_ID', 'PAGE' => 'line/html'],
-        'GET /([a-z]+)/([0-9]+)/pdf' => ['LINETYPE_NAME', 'LINE_ID', 'PAGE' => 'line/pdf'],
+        'GET /([a-z]+)/([A-Z0-9]+)' => ['LINETYPE_NAME', 'LINE_ID', 'PAGE' => 'line/index'],
+        'GET /([a-z]+)/([A-Z0-9]+)/html' => ['LINETYPE_NAME', 'LINE_ID', 'PAGE' => 'line/html'],
+        'GET /([a-z]+)/([A-Z0-9]+)/pdf' => ['LINETYPE_NAME', 'LINE_ID', 'PAGE' => 'line/pdf'],
         'GET /([a-z]+)/info' => ['LINETYPE_NAME', 'PAGE' => 'line/info'],
         'GET /([a-z]+)/suggested' => ['LINETYPE_NAME', 'PAGE' => 'line/suggested'],
         'GET /blend/([a-z]+)/info' => ['BLEND_NAME', 'PAGE' => 'blend/info'],
@@ -24,7 +24,7 @@ class Router extends \Router
         'POST /auth/login' => ['PAGE' => 'login', 'NOAUTH' => true],
         'POST /auth/logout' => ['PAGE' => 'logout'],
         'POST /([a-z]+)' => ['LINETYPE_NAME', 'PAGE' => 'line/save'],
-        'POST /([a-z]+)/([0-9]+)/unlink/([a-z]+_[a-z]+)' => ['LINETYPE_NAME', 'LINE_ID', 'PARNT', 'PAGE' => 'line/unlink'],
+        'POST /([a-z]+)/([A-Z0-9]+)/unlink/([a-z]+_[a-z]+)' => ['LINETYPE_NAME', 'LINE_ID', 'PARNT', 'PAGE' => 'line/unlink'],
         'POST /([a-z]+)/print' => ['LINETYPE_NAME', 'PAGE' => 'line/print'],
     ];
 }
