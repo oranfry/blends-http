9a0cd1d8065cf4d1188af8b74d5658e23698879f
Oran Fry <oran@emerg.co>
2020-09-27 18:45:29 +1300

    Save Line
    Conform to new blends api method signature.

--- src/php/controller/line/save.php
+++ src/php/controller/line/save.php
@@ -4,5 +4,5 @@ $lines = json_decode(file_get_contents('php://input'));
 $keep_filedata = @$_GET['keepfiledata'] === '1';
 
 return [
-    'data' => $linetype->save(AUTH_TOKEN, $lines, 0, null, $keep_filedata),
+    'data' => $linetype->save(AUTH_TOKEN, $lines, null, $keep_filedata),
 ];
