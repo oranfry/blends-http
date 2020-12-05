00ad947b21a299dcfd6c3a863bd1e28d5423c4b6
Oran Fry <oran@emerg.co>
2020-12-05 13:28:00 +1300

    Public URLs
    Apply security which is no longer provided by subsimple.

--- src/php/script/lib.php
+++ src/php/script/lib.php
@@ -2,6 +2,17 @@
 
 define('AUTH_TOKEN', @getallheaders()['X-Auth']);
 
+function init_app()
+{
+    if (
+        (!preg_match(',^/auth/(login|logout)$,', $_SERVER['REQUEST_URI']) || $_SERVER['REQUEST_METHOD'] != 'POST')
+        &&
+        !Blends::verify_token(AUTH_TOKEN)
+    ) {
+        error_response('Unauthorised');
+    }
+}
+
 function get_query_filters()
 {
     $filters = [];
